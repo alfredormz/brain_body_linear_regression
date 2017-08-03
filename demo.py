475b73ff2e849e78002f7d526c2b5e17c8fd8164
Alfredo Ramírez <alfredormz@gmail.com>
2017-08-02 23:51:44 -0300

    First commit

---
+++ demo.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_fwf('brain_body.txt')
+x_values = dataframe[['Brain']]
+y_values = dataframe[['Body']]
+
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values, y_values)
+
+plt.scatter(x_values, y_values)
+plt.plot(x_values, body_reg.predict(x_values))
+plt.show()
